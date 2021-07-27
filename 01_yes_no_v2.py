3fda6e4285eb7f8056f78dfbd35b4d8a49f15ffc
greennsarahh <green.sarah@awatapu.school.nz>
2021-07-27 15:04:10 +1200

    change

--- 01_yes_no_v2.py
+++ 01_yes_no_v2.py
@@ -4,17 +4,18 @@ while show_instructions.lower() != "xxx":
     # ask the user if they have played before
     show_instructions = input("Have you played this game before?").lower().strip()
     # if they say yes, output 'program continues'
-    if show_instructions == "yes":
+    if show_instructions == "yes" or show_instructions == "y":
+        show_instructions = "yes"
         print("program continues")
 
     elif show_instructions == "y":
         print("program continues")
 
-    elif show_instructions == "no":
-        print("display instructions")
+    elif show_instructions == "no" or show_instructions == "n":
+        print("program continues")
 
     elif show_instructions == "n":
-        print("display instructions")
+        print("program continues")
 
     # if they say no, output 'display instructions'
     else:
