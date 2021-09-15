74b90800fa62a60c8ecba765ae07bf2e52950b66
greennsarahh <green.sarah@awatapu.school.nz>
2021-09-15 15:03:22 +1200

    ytftrytr

--- venv/00_base_v1.py
+++ venv/00_base_v1.py
@@ -53,6 +53,44 @@ if played_before == "no":
     instruction()
 print("Program continues")
 
-
 how_much = num_check ("How much would you like to play with? ", 0, 10)
 print("You will be spending ${}" .format(how_much))
+balance = how_much
+
+play_again = input("Press <enter> to play...").lower()
+while play_again == "":
+
+    # increase number of rounds played
+    rounds_played += 1
+
+    # print rounds number
+    print()
+    print("*** Rounds #{} ***".format(rounds_played))
+
+    chosen_num = random.randint(1, 100)
+    # adjust balance
+    # if random # is between 1 and 5
+    # user gets unicorn (add 4$ to balance)
+    if 1 <= chosen_num <= 5:
+        chosen = "unicorn"
+        balance += 4
+        # if the random num is between 6 and 36
+        # user gets donkey (subtract 1$ from balance)
+    elif 6 <= chosen_num <= 36:
+        chosen = "donkey"
+        balance -= 1
+    else:
+        if chosen_num % 2 == 0:
+            chosen = "horse"
+        else:
+            chosen = "zebra"
+            balance -= 0.5
+
+    print("You got a {}. Your balance is ${:.2f}".format(chosen, balance))
+    print()
+
+    if balance < 1:
+        play_again = "xxx"
+        print("Sorry you have run out of money")
+    else:
+        play_again = input("Press enter to play again or 'xxx' to quit")
