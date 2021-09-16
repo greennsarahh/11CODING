22fb5d5a31effdaca3116f162d685d16b38be7f6
greennsarahh <green.sarah@awatapu.school.nz>
2021-09-17 09:34:57 +1200

    final game working

--- venv/00_base_v1.py
+++ venv/00_base_v1.py
@@ -1,11 +1,12 @@
+import random
 # Functions
 def num_check(question, low, high):
-    error = "Please eneter a whole number beetween 1 and 10\n"
+    error = "Please enter a whole number between 1 and 10\n"
 
     valid = False
     while not valid:
         try:
-            #ask the question
+            # ask the question
             response = int(input(question))
 
             # if the amount is too low/ high give
@@ -45,7 +46,7 @@ def instruction():
 
 
 # main routine goes here
-
+rounds_played = 0
 played_before = yes_no("Have you played the game before?")
 print("You chose {}" .format(played_before))
 print()
@@ -53,7 +54,7 @@ if played_before == "no":
     instruction()
 print("Program continues")
 
-how_much = num_check ("How much would you like to play with? ", 0, 10)
+how_much = num_check("How much would you like to play with? ", 0, 10)
 print("You will be spending ${}" .format(how_much))
 balance = how_much
 
